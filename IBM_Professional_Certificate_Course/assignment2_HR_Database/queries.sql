4eedecc1e6b76caea59b4cd884b3b3468fe3107e
Soumya Deep Chatterjee <44434633+chatterjeesd@users.noreply.github.com>
2018-12-27 20:26:57 +0100

    Update queries.sql

--- IBM_Professional_Certificate_Course/assignment2_HR_Database/queries.sql
+++ IBM_Professional_Certificate_Course/assignment2_HR_Database/queries.sql
@@ -1,62 +1,66 @@
--- Query 1------
+-- Query 1 Retrieve all employees whose address is in Elgin,IL
 ;
 select F_NAME , L_NAME
 from EMPLOYEES
 where ADDRESS LIKE '%Elgin,IL%' ;
---Query 2--
+--Query 2: Retrieve all employees who were born during the 1970's.
 ;
 select F_NAME , L_NAME
 from EMPLOYEES
 where B_DATE LIKE '197%' ;
----Query3--
+---Query 3: Retrieve all employees in department 5 whose salary is between 60000 and 70000 .
 ;
 select *
 from EMPLOYEES
 where (SALARY BETWEEN 60000 and 70000)  and DEP_ID = 5 ;
---Query4A--
+--Query 4A: Retrieve a list of employees ordered by departmentID.
 ;
 select F_NAME, L_NAME, DEP_ID 
 from EMPLOYEES
 order by DEP_ID;
---Query4B--
+--Query 4B: Retrieve a list of employees ordered in descending 
+--order by departmentIDand within each department ordered alphabetically in descending order by last name.
 ;
 select F_NAME, L_NAME, DEP_ID 
 from EMPLOYEES
 order by DEP_ID desc, L_NAME desc;
---Query5A--
+--Query 5A: For each department ID retrieve thenumberof employees in the department.
 ;
 select DEP_ID, COUNT(*)
 from EMPLOYEES
 group by DEP_ID;
---Query5B--
+--Query 5B: For each department retrieve thenumber of employees in thedepartment, and theaverage employees salaryin the department.
 ;
 select DEP_ID, COUNT(*), AVG(SALARY)
 from EMPLOYEES
 group by DEP_ID;
---Query5C--
+--Query 5C: Label the computed columns in the result set of Query 5B as “NUM_EMPLOYEES”and “AVG_SALARY”.
 ;
 select DEP_ID, COUNT(*) AS "NUM_EMPLOYEES", AVG(SALARY) AS "AVG_SALARY"
 from EMPLOYEES
 group by DEP_ID;
---Query5D--
+--Query 5D: In Query 5C order the result set by Average Salary.
 ;
 select DEP_ID, COUNT(*) AS "NUM_EMPLOYEES", AVG(SALARY) AS "AVG_SALARY"
 from EMPLOYEES
 group by DEP_ID
 order by AVG_SALARY;
---Query5E--
+--Query 5E: In Query 5Dlimitthe result to departments withfewerthan 4 employees.
 ;
 select DEP_ID, COUNT(*) AS "NUM_EMPLOYEES", AVG(SALARY) AS "AVG_SALARY"
 from EMPLOYEES
 group by DEP_ID
 having count(*) < 4
 order by AVG_SALARY;
+
 --5E alternative: if you want to use the label
 select DEP_ID, NUM_EMPLOYEES, AVG_SALARY from
   ( select DEP_ID, COUNT(*) AS NUM_EMPLOYEES, AVG(SALARY) AS AVG_SALARY from EMPLOYEES group by DEP_ID)
   where NUM_EMPLOYEES < 4
 order by AVG_SALARY;
---BONUS Query6--
+
+--Query 6: Similar to 4B but instead of department ID use department name. Retrieve a list of employees ordered by 
+--department name, and within each department ordered alphabetically in descending order by last name.
 ;
 select D.DEP_NAME , E.F_NAME, E.L_NAME
 from EMPLOYEES as E, DEPARTMENTS as D
